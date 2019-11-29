fb21efd136f6b906c18a3b422252b5fe78f3db3b
Vancia <vancia.colleoni@me.com>
2019-11-29 14:08:22 +0100

    fix #2 Tests sur QuestionAChoixMultiple

---
+++ src/test/java/miagem1/QuestionAChoixMultipleTest.java
@@ -0,0 +1,47 @@
+package miagem1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class QuestionAChoixMultipleTest {
+
+    private QuestionAChoixMultiple question;
+
+    @Before
+    public void setUp() throws Exception {
+        // given : une instance de question à choix multiple
+        ArrayList listRep = new ArrayList<Integer>();
+        listRep.add(1);
+        listRep.add(3);
+        question = new QuestionAChoixMultiple("énoncé", listRep);
+    }
+
+    @Test
+    public void testGetEnonce() {
+        // when: on demande l'énoncé de la question
+        String enonce = question.getEnonce();
+        // then: l'énoncé retourné est l'énoncé fourni à la construction de l'énoncé
+        assertEquals("énoncé", enonce);
+    }
+
+    @Test
+    public void getScoreForIndice() {
+        // when: quand l'étudiant donne l'indice d'une bonne réponse
+        int indice = 1;
+        // and: on demande le score de l'étudiant
+        float score = question.getScoreForIndice(indice);
+        // then: le score obtenu est de 50
+        assertEquals(score, 50f, 0.01f);
+
+        // when: quand l'étudiant donne l'indice d'une mauvaise réponse
+        int indiceM = 2;
+        // and: on demande le score de l'étudiant
+        float scoreM = question.getScoreForIndice(indiceM);
+        // then: le score obtenu est de 0
+        assertEquals(scoreM, 0f, 0.01f);
+    }
+}
